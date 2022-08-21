a4fa511a71aee1adfbcd4e38ccd31f870b52f087
chutki12 <priyankachaudhary2068>
2022-08-21 11:42:28 +0530

    ADDED : Welcome To Address Book Management Sysyem

---
+++ src/main/java/com/bridgelabz/addressBookSystem/AddressBookSystemMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressBookSystem;
+
+public class AddressBookSystemMain {
+
+	public static void main(String[] args) {
+		
+		 System.out.println(" welcome To Address Book Management system");
+
+	}
+
+}
